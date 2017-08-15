cab8bbc8252263f5e7f084f2e7da3780cc5c88fe
Raymond <raymondpreston8@gmail.com>
2017-08-14 19:18:11 -0500

    Fixed most things
    Only thing that isn't working currently is switching accounts, so I am capped at one but I should find a fix for it soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 from accounts import *
+import logging
 
 
 #Upvote_Users says who you want to upvote. Do caps matters?
@@ -10,7 +11,8 @@ downvote_users = ("Coni_s2", "sintralin", "Crimeo")
 def vote_posts(reddit_account):
 #You can add as many reddit accounts to this as you want, just make sure to call them later on with the account
     "Downvotes posts or upvotes posts accordingly to who you want to downvote/upvote"
-    for submission in reddit_account.subreddit("Civclassics").new():
+    subreddit = reddit_account.subreddit("Civclassics").new()
+    for submission in subreddit:
         if submission.author in downvote_users:
             submission.downvote()
             print("I downvoted {}, posted by {}".format(submission.title, submission.author))
@@ -18,5 +20,19 @@ def vote_posts(reddit_account):
             submission.upvote()
             print("I upvoted {}, posted by {}".format(submission.title, submission.author))
 
-vote_posts(Searednumber2)
-vote_posts(KillAllTrolls)
+def vote_on_comments(reddit_account):
+    "Downvotes or upvotes comments on posts accordingly"
+    subreddit = reddit_account.subreddit("Civclassics").stream.comments()
+    for comment in subreddit:
+        if comment.author in upvote_users:
+            comment.upvote()
+            print("I just upvoted {}. The text body was '{}'".format(comment.author, comment.body))
+        elif comment.author in downvote_users:
+            comment.downvote()
+            print("I just downvoted {}. The text body was '{}'".format(comment.author, comment.body))
+
+#vote_posts(SearedNumber2)
+#vote_on_comments(SearedNumber2)
+
+vote_posts(KillAllTrolls)       
+vote_on_comments(KillAllTrolls)
